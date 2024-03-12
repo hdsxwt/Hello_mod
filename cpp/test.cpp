74b6ae1d7348b4155f78def6f525b27537ad920a
hdsxwt <159417501+hdsxwt@users.noreply.github.com>
2024-03-12 14:12:17 +0800

    Update test.cpp

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -7,8 +7,8 @@ using namespace std;
 ofstream out;
 
 const LD max_v = 100;
-const LD max_a = 10;
-const LD F_max = 10;
+const LD max_a = 5;
+const LD F_max = 5;
 const LD f_max = 5;
 const LD mv = 1;
 const LD p1 = 1;
@@ -16,10 +16,10 @@ const LD p2 = 0;
 int k;
 LD v;
 LD t;
-const LD k1 = 1;
-const LD k2 = 1;
-const LD k3 = 0.5;
-const LD k4 = 0.01;
+const LD k1 = 1; // ¶¯Á¦ 
+const LD k2 = 2; // ·ÉÂÖ 
+const LD k3 = 0.5; // É²³µ 
+const LD k4 = 0.01; // ·ç×è 
 
 void line(const string text, const int &p) {
 	if (p > 100 || p < 0) {
@@ -49,17 +49,17 @@ LD f1(LD k) {
 	return 1/(log2((LD)k/2+1.5));
 }
 LD f2(LD k) {
-	return pow(2, 0.01*k) - 1;
+	return pow(2, 0.04*k) - 1;
 }
 
 int main() {
 	
-	out.open("out.csv");
+	out.open("out1.csv");
 	out << "t,v,a\n";
 	k = 1;
 	v = 0;
 	t = 0;
-	int cnt = 10;
+	int cnt = 60;
 	cnt *= 20;
 	while (cnt--) {
 		LD a1 = k1 * mv * F_max * p1 * f1(k);
@@ -73,4 +73,5 @@ int main() {
 		t += 0.05;
 //		Sleep(50);
 	}
+	out.close();
 }
